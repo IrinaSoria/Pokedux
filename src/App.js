0960c946d3fd7e7f630e84233f5b8f9841181b4b
soria.irina <damarisirinasoria@gmail.com>
2024-05-13 23:10:38 -0300

    inmutabilidad

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import { useEffect } from 'react';
-import { Col, Spin } from 'antd';
-import { useDispatch, useSelector } from 'react-redux';
-import Searcher from './components/Searcher';
-import PokemonList from './components/PokemonList';
-import { getPokemon } from './api';
-import { getPokemonsWithDetails, setLoading } from './actions';
-import logo from './statics/logo.svg';
-import './App.css';
-
+import { useEffect } from "react";
+import { Col, Spin } from "antd";
+import { useDispatch, useSelector } from "react-redux";
+import Searcher from "./components/Searcher";
+import PokemonList from "./components/PokemonList";
+import { getPokemon } from "./api";
+import { getPokemonsWithDetails, setLoading } from "./actions";
+import logo from "./statics/logo.svg";
+import "./App.css";
 function App() {
-  const pokemons = useSelector((state) => state.pokemons);
-  const loading = useSelector((state) => state.loading);
+  const pokemons = useSelector((state) => state.get("pokemons")).toJS();
+  const loading = useSelector((state) => state.get("loading"));
   const dispatch = useDispatch();
-
   useEffect(() => {
     const fetchPokemons = async () => {
       dispatch(setLoading(true));
@@ -20,21 +18,19 @@ function App() {
       dispatch(getPokemonsWithDetails(pokemonsRes));
       dispatch(setLoading(false));
     };
-
     fetchPokemons();
   }, []);
-
   return (
-    <div className='App'>
+    <div className="App">
       <Col span={4} offset={10}>
-        <img src={logo} alt='Pokedux' />
+        <img src={logo} alt="Pokedux" />
       </Col>
       <Col span={8} offset={8}>
         <Searcher />
       </Col>
       {loading ? (
         <Col offset={12}>
-          <Spin spinning size='large' />
+          <Spin spinning size="large" />
         </Col>
       ) : (
         <PokemonList pokemons={pokemons} />
@@ -42,5 +38,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
